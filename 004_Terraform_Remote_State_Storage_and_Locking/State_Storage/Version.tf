e52d785926902c055360205e2b9d7d2a05a3d33a
kavya <kavyareddy41997@gmail.com>
2023-01-10 20:05:26 -0700

    Updated

---
+++ 004_Terraform_Remote_State_Storage_and_Locking/State_Storage/Version.tf
@@ -0,0 +1,36 @@
+# Terraform Settings Block
+terraform {
+  # Terraform Version
+  required_version = "~> 1.3.7"
+  required_providers {
+    # AWS Provider 
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+
+  }
+#Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+  
+    bucket = "terraform-kavya-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-west-2"
+   
+    
+  }
+  
+  
+}
+
+
+
+
+
+
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default" # Defining it for default profile is Optional
+}
